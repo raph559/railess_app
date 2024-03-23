e84f7aafd52b46fff2a6ed2637127127a4923ab9
Rafael Drouart <rafael.drouart@epitech.eu>
2024-03-23 20:32:01 +0100

    [~] Displaying the change color as a button in the bottom left of the menu bar

--- lib/my_drawer.dart
+++ lib/my_drawer.dart
@@ -22,93 +22,95 @@ class MyDrawer extends StatelessWidget {
     return Drawer(
       child: Container(
         color: CustomColors.background,
-        child: ListView(
-          padding: EdgeInsets.zero,
+        child: Column(
           children: <Widget>[
-            DrawerHeader(
-              child: Text(
-                'Railess.',
-                style: TextStyle(
-                  fontSize: 50.0,
-                  fontWeight: FontWeight.w700,
-                  fontFamily: 'NotoSansMono',
-                  color: isDark ? Colors.black : Colors.white,
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: <Widget>[
+                  DrawerHeader(
+                    child: Text(
+                      'Railess.',
+                      style: TextStyle(
+                        fontSize: 50.0,
+                        fontWeight: FontWeight.w700,
+                        fontFamily: 'NotoSansMono',
+                        color: isDark ? Colors.black : Colors.white,
+                      ),
+                    ),
+                  ),
+                  ListTile(
+                    title: Text(
+                      'About',
+                      style: TextStyle(
+                        color: isDark ? Colors.black : Colors.white,
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const AboutPage()),
+                      );
+                    },
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Service',
+                      style: TextStyle(
+                        color: isDark ? Colors.black : Colors.white,
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const ServicePage()),
+                      );
+                    },
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Help',
+                      style: TextStyle(
+                        color: isDark ? Colors.black : Colors.white,
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const HelpPage()),
+                      );
+                    },
+                  ),
+                  ListTile(
+                    title: Text(
+                      'Contact',
+                      style: TextStyle(
+                        color: isDark ? Colors.black : Colors.white,
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const ContactPage()),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Container( // Use a Container instead of DrawerFooter
+              alignment: Alignment.bottomLeft,
+              child: IconButton(
+                color: isDark ? Colors.black : Colors.white,
+
+                icon: Icon(isDark ? Icons.brightness_2 : Icons.brightness_7
                 ),
+                onPressed: () => onThemeChanged(!isDark),
               ),
             ),
-            ListTile(
-              title: Text(
-                'About',
-                style: TextStyle(
-                  color: isDark ? Colors.black : Colors.white,
-                ),
-              ),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const AboutPage()),
-                );
-              },
-            ),
-            ListTile(
-              title: Text(
-                'Service',
-                style: TextStyle(
-                  color: isDark ? Colors.black : Colors.white,
-                ),
-              ),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const ServicePage()),
-                );
-              },
-            ),
-            ListTile(
-              title: Text(
-                'Help',
-                style: TextStyle(
-                  color: isDark ? Colors.black : Colors.white,
-                ),
-              ),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const HelpPage()),
-                );
-              },
-            ),
-            ListTile(
-              title: Text(
-                'Contact',
-                style: TextStyle(
-                  color: isDark ? Colors.black : Colors.white,
-                ),
-              ),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const ContactPage()),
-                );
-              },
-            ),
-            ListTile(
-              title: Text(
-              'Dark Mode',
-              style: TextStyle(
-              color: isDark ? Colors.black : Colors.white,
-              ),
-            ),
-            trailing: Switch(
-              value: isDark,
-              onChanged: onThemeChanged,
-              activeColor: Colors.black,
-              inactiveThumbColor: Colors.white,
-            ),
-            ),
-         ],
+          ],
+        ),
       ),
-    ),
     );
   }
 }
